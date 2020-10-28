a2730755764fca5093eefa188b58e501d98c9505
tanishk2298 <tsrsharma.22@gmail.com>
2020-10-28 18:49:17 +0530

    connect mongodb

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express(); 
 
+const connectDB = require('./config/db')
+connectDB();
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
