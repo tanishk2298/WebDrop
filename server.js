45ed41f677f8e05b37ae7477d2cfee5130fb4949
tanishk2298 <tsrsharma.22@gmail.com>
2020-10-29 23:02:52 +0530

    file download

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express(); 
+const path = require('path')
 
 const connectDB = require('./config/db')
 connectDB();
 
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
 app.use('/api/files', require('./routes/files'))
+app.use('/files', require('./routes/show'))
 
 const PORT = process.env.PORT || 3000
 
