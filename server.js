c00f50d9232dba057dc4ad01977f37cd754257a8
tanishk2298 <tsrsharma.22@gmail.com>
2020-10-31 16:09:57 +0530

    cors setup

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express(); 
 const path = require('path')
+const cors = require('cors')
 
 app.use(express.static('public'))
 app.use(express.json())
 
 const connectDB = require('./config/db')
 connectDB();
+
+const corsOptions = {
+    orgin : ['http://localhost:3000', 'http://localhost:5000']
+}
+app.use(cors(corsOptions))
 
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
