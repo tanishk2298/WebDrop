418d417a041318a10870cf5585d37fd7aeae8523
tanishk2298 <tsrsharma.22@gmail.com>
2020-11-01 22:43:36 +0530

    drag and upload

--- client/index.js
+++ client/index.js
@@ -1,4 +1,9 @@
 const dropzone = document.querySelector(".drop-zone");
+const fileInp = document.querySelector("#fileInp");
+const browse = document.querySelector(".browseBtn");
+
+const host = "https://webdrop-fileshare.herokuapp.com/";
+const uploadURL = `${host}api/files`;
 
 dropzone.addEventListener("dragover", (e) => {
     e.preventDefault();
@@ -14,4 +19,35 @@ dropzone.addEventListener("dragleave", (e) => {
 dropzone.addEventListener("drop", (e) => {
     e.preventDefault();
     dropzone.classList.remove("dragged");
+    const files = e.dataTransfer.files;
+    if(files.length){
+        fileInp.files = files;
+        uploadFile();
+    }
 })
+
+fileInp.addEventListener("change", () => {
+    uploadFile();
+})
+
+browse.addEventListener("click", () => {
+    fileInp.click();  
+})
+
+
+const uploadFile = () => {
+    const xhr = new XMLHttpRequest();
+    const file = fileInp.files[0];
+    const formData = new FormData();
+    formData.append("myfile", file);
+
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            console.log(xhr.response);
+        }
+    }
+
+    xhr.open("POST", uploadURL);
+    xhr.send(formData);
+}
+
