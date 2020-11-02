3958c9d49b94f30d4143f3ab847aa5fd3ddcb47e
tanishk2298 <tsrsharma.22@gmail.com>
2020-11-02 14:47:49 +0530

    add fileupload progress section

--- client/index.js
+++ client/index.js
@@ -1,6 +1,10 @@
 const dropzone = document.querySelector(".drop-zone");
 const fileInp = document.querySelector("#fileInp");
 const browse = document.querySelector(".browseBtn");
+const bgprogress = document.querySelector(".bg-progress");
+const percentDiv = document.querySelector("#percent");
+const progressContainer = document.querySelector(".progress-container");
+const uploading = document.querySelector(".uploading");
 
 const host = "https://webdrop-fileshare.herokuapp.com/";
 const uploadURL = `${host}api/files`;
@@ -19,6 +23,9 @@ dropzone.addEventListener("dragleave", (e) => {
 dropzone.addEventListener("drop", (e) => {
     e.preventDefault();
     dropzone.classList.remove("dragged");
+    uploading.innerText = "Uploading...";
+    bgprogress.style.width = "0%";
+    percentDiv.innerText = 0;
     const files = e.dataTransfer.files;
     if(files.length){
         fileInp.files = files;
@@ -36,6 +43,7 @@ browse.addEventListener("click", () => {
 
 
 const uploadFile = () => {
+    progressContainer.style.display = "block";
     const xhr = new XMLHttpRequest();
     const file = fileInp.files[0];
     const formData = new FormData();
@@ -43,11 +51,26 @@ const uploadFile = () => {
 
     xhr.onreadystatechange = () => {
         if(xhr.readyState === XMLHttpRequest.DONE){
-            console.log(xhr.response);
+            const link = JSON.parse(xhr.response);
+            console.log(link.file);
+            progressContainer.style.display = "none";
         }
     }
+
+    xhr.upload.onprogress = updateProgress;
 
     xhr.open("POST", uploadURL);
     xhr.send(formData);
 }
+
+const updateProgress = (e) => {
+    const percent = Math.round((e.loaded / e.total) * 100);
+    if(percent === 100){
+        uploading.innerText = "Uploaded"
+    }
+    bgprogress.style.width = `${percent}%`;
+    percentDiv.innerText = percent;
+}
+
+
 
